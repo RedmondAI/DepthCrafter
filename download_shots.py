364e37c3e281974cea09f64783bfcf6422c24941
Redmond-AI <126352754+Redmond-AI@users.noreply.github.com>
2024-09-16 16:50:18 -0400

    Update download_shots.py

--- download_shots.py
+++ download_shots.py
@@ -1,7 +1,6 @@
 import os
 import boto3
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import argparse
 
 # S3 Configuration
 AWS_REGION = ""
@@ -10,7 +9,7 @@ S3_ACCESS_KEY = "283b8c4c028487ff3e1bbc2a891189e7"
 S3_SECRET_KEY = "bc3d3ee909929216aa16c764d85e9b743bbc91e2589295955bda9624d0afdd21"
 DEFAULT_ENDPOINT = "47ec7d0d5b6a6c2bcda5211d2d412fd0.r2.cloudflarestorage.com"
 S3_ROOT_URL = "https://pub-da6ae3cf12bc4de49e659943f4080da6.r2.dev/"
-#START_PREFIX = "reel01/"
+START_PREFIX = "quietplace/"
 
 # Initialize S3 client
 s3_client = boto3.client(
@@ -44,24 +43,15 @@ def download_rgb_deflicker(root_prefix):
     for obj in response['Contents']:
         key = obj['Key']
         filename = os.path.basename(key)
-        local_path = os.path.join("input_"+root_prefix, root_folder, filename)
-        
-        # Check if the file already exists locally
-        if os.path.exists(local_path):
-            print(f"File {local_path} already exists, skipping download.")
-            continue
-        
+        local_path = os.path.join("input2",root_folder, filename)
         s3_client.download_file(S3_BUCKET_NAME, key, local_path)
         print(f"Downloaded {key} to {local_path}")
 
 def main():
-    parser = argparse.ArgumentParser(description='Download RGB deflicker files from S3')
-    parser.add_argument('--start_prefix', type=str, help='The prefix to start listing directories from')
-    args = parser.parse_args()
-    directories = list_directories(args.start_prefix)
+    directories = list_directories(START_PREFIX)
     for dir in directories:
         print("dir", dir)
-    with ThreadPoolExecutor(max_workers=8) as executor:
+    with ThreadPoolExecutor(max_workers=4) as executor:
         futures = [executor.submit(download_rgb_deflicker, d) for d in directories]
         for future in as_completed(futures):
             future.result()
